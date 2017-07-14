0e03854ac723733f3a8475cb32bdb3a946626fab
maribelvm <m.velasquezmoscoso@gmail.com>
2017-07-14 04:28:24 -0700

    Ejercicio2

--- ejercicios.js
+++ ejercicios.js
@@ -1,1 +1,13 @@
 //AGREGAR SUS FUNCIONES
+function Ejercicio2(n){
+
+  var capicua = n.toString().split("").reverse().toString().replace(/,/g,"");
+  var c = n.toString();
+
+  if(c == capicua){
+    return "Es capicua"
+  } else{
+    return "No es capicua"
+  }
+}
+Ejercicio2(989);
