065868e52ceb848023abef14ac666b3a01ebf670
Sharmyn28 <sharmynrodriguez@gmail.com>
2017-07-14 16:23:43 -0500

    Mi funcion mili

--- ejercicios.js
+++ ejercicios.js
@@ -1,1 +1,18 @@
 //AGREGAR SUS FUNCIONES
+
+ function mili(_arr) {
+
+    const nuevo ={
+      inicio: 1,
+      fin : 10,
+      sumar: function (){
+        var sum = 0;
+        for(var i = this.inicio; i <= this.fin; i++){
+          sum += i;
+        }
+        return sum;
+      }
+    }
+    
+    return nuevo.sumar();
+  }
