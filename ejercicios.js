f1fef9e971238fbfc229c6402c5cb09bddb32a45
anaYesi <anayesenia026@gmail.com>
2017-07-14 04:30:42 -0500

    agrege el ejercicio 7

--- ejercicios.js
+++ ejercicios.js
@@ -1,1 +1,17 @@
+//que aparescas los multiplos de un numero
+function ejercicio7(arreglo, numero)
+{
+	var  resultado =[];
+	//forEach solo recibe un parametro
+	arreglo.forEach(function (a){
+		if( a%numero == 0)
+		{
+			//push va mandar los parametros con comas
+			resultado.push(a); //push agrega mas elementos al final de un array
+		}
+	});
+	return resultado;
+}
+
+document.write(ejercicio7([7,21,44,80,77,35],7));
 //AGREGAR SUS FUNCIONES
